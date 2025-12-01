d05f712ace10cf2eaea0a36a465150dba4f6257b
Rui1818 <ruidirui@sms31-43-197.ethz.ch>
2025-12-01 10:50:11 +0100

    added training

--- run_gait_training.sh
+++ run_gait_training.sh
@@ -5,7 +5,7 @@
 DATASET_PATH="mydataset"
 DATASET_NAME="gait"
 BATCH_SIZE=8
-SAVE_INTERVAL=5000
+SAVE_INTERVAL=2000
 LOG_INTERVAL=1000
 DEVICE=0
 NUM_WORKERS=4
@@ -71,48 +71,37 @@ run_training() {
 # --- Experiment Definitions ---
 # Now, we just call the function for each configuration we want to test.
 # The script will run these one after another.
-    local save_dir=$1
-    local latent_dim=$2
-    local layers=$3
-    local weight_decay=$4
-    local num_steps=$5
-    local lr=$6
-    local keypointtype=$7
-    local cond_mask_prob=$8
-    local lambda_rot_vel=$9
-    local lambda_transl_vel=${10}
-    local motionnfeatures=${11}
 
 # Openpose keypoints
 
-run_training "my_training/config1" 256 6 1e-4 500000 2e-4 openpose 0 0 0 69
-run_training "my_training/config2" 512 6 1e-4 500000 2e-4 openpose 0 0 0 69
-run_training "my_training/config3" 256 8 1e-4 500000 2e-4 openpose 0 0 0 69
-run_training "my_training/config4" 512 8 1e-4 500000 2e-4 openpose 0 0 0 69
-run_training "my_training/config5" 256 4 1e-4 500000 2e-4 openpose 0 0 0 69
-run_training "my_training/config6" 512 4 1e-4 500000 2e-4 openpose 0 0 0 69
+#run_training "my_training/config1" 256 6 1e-4 400000 2e-4 openpose 0 0 0 69
+#run_training "my_training/config2" 512 6 1e-4 400000 2e-4 openpose 0 0 0 69
+#run_training "my_training/config3" 256 8 1e-4 400000 2e-4 openpose 0 0 0 69
+#run_training "my_training/config4" 512 8 1e-4 400000 2e-4 openpose 0 0 0 69
+#run_training "my_training/config5" 256 4 1e-4 400000 2e-4 openpose 0 0 0 69
+#run_training "my_training/config6" 512 4 1e-4 400000 2e-4 openpose 0 0 0 69
 #velocity
-run_training "my_training/config7" 256 6 1e-4 500000 2e-4 openpose 0 0.5 0 69
-run_training "my_training/config8" 512 6 1e-4 500000 2e-4 openpose 0 0.5 0 69
-run_training "my_training/config9" 256 8 1e-4 500000 2e-4 openpose 0 0.5 0 69
-run_training "my_training/config10" 512 8 1e-4 500000 2e-4 openpose 0 0.5 0 69
-run_training "my_training/config11" 256 4 1e-4 500000 2e-4 openpose 0 0.5 0 69
-run_training "my_training/config12" 512 4 1e-4 500000 2e-4 openpose 0 0.5 0 69
+run_training "my_training/config7" 256 6 1e-4 200000 2e-4 openpose 0 0.5 0 69
+run_training "my_training/config8" 512 6 1e-4 200000 2e-4 openpose 0 0.5 0 69
+run_training "my_training/config9" 256 8 1e-4 200000 2e-4 openpose 0 0.5 0 69
+run_training "my_training/config10" 512 8 1e-4 200000 2e-4 openpose 0 0.5 0 69
+run_training "my_training/config11" 256 4 1e-4 200000 2e-4 openpose 0 0.5 0 69
+run_training "my_training/config12" 512 4 1e-4 200000 2e-4 openpose 0 0.5 0 69
 
 # 6d rotations
-run_training "my_training/config13" 256 6 1e-4 500000 2e-4 6d 0 0 0 135
-run_training "my_training/config14" 512 6 1e-4 500000 2e-4 6d 0 0 0 135
-run_training "my_training/config15" 256 8 1e-4 500000 2e-4 6d 0 0 0 135
-run_training "my_training/config16" 512 8 1e-4 500000 2e-4 6d 0 0 0 135
-run_training "my_training/config17" 256 4 1e-4 500000 2e-4 6d 0 0 0 135
-run_training "my_training/config18" 512 4 1e-4 500000 2e-4 6d 0 0 0 135
+run_training "my_training/config13" 256 6 1e-4 200000 2e-4 6d 0 0 0 135
+run_training "my_training/config14" 512 6 1e-4 200000 2e-4 6d 0 0 0 135
+run_training "my_training/config15" 256 8 1e-4 200000 2e-4 6d 0 0 0 135
+run_training "my_training/config16" 512 8 1e-4 200000 2e-4 6d 0 0 0 135
+run_training "my_training/config17" 256 4 1e-4 200000 2e-4 6d 0 0 0 135
+run_training "my_training/config18" 512 4 1e-4 200000 2e-4 6d 0 0 0 135
 #velocity
-run_training "my_training/config19" 256 6 1e-4 500000 2e-4 6d 0 0.5 0.1 135
-run_training "my_training/config20" 512 6 1e-4 500000 2e-4 6d 0 0.5 0.1 135
-run_training "my_training/config21" 256 8 1e-4 500000 2e-4 6d 0 0.5 0.1 135
-run_training "my_training/config22" 512 8 1e-4 500000 2e-4 6d 0 0.5 0.1 135
-run_training "my_training/config23" 256 8 1e-4 500000 2e-4 6d 0 0.5 0.1 135
-run_training "my_training/config24" 512 8 1e-4 500000 2e-4 6d 0 0.5 0.1 135
+run_training "my_training/config19" 256 6 1e-4 200000 2e-4 6d 0 0.5 0.1 135
+run_training "my_training/config20" 512 6 1e-4 200000 2e-4 6d 0 0.5 0.1 135
+run_training "my_training/config21" 256 8 1e-4 200000 2e-4 6d 0 0.5 0.1 135
+run_training "my_training/config22" 512 8 1e-4 200000 2e-4 6d 0 0.5 0.1 135
+run_training "my_training/config23" 256 8 1e-4 200000 2e-4 6d 0 0.5 0.1 135
+run_training "my_training/config24" 512 8 1e-4 200000 2e-4 6d 0 0.5 0.1 135
 
 # Config 4: Add more configs as you like...
 # run_training "my_training/config4_..." ... ... ...
